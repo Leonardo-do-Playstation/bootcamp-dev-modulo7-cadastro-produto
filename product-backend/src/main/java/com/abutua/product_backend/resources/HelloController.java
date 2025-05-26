61e14043b32bc10c935f6c9cae4e7cae16fc056b
Leonardo <leooliveiraboff@gmail.com>
2025-05-26 16:50:38 -0300

    first commit

---
+++ product-backend/src/main/java/com/abutua/product_backend/resources/HelloController.java
@@ -0,0 +1,12 @@
+package com.abutua.product_backend.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping ("hello")
+    public String gethello(){
+        return "Hello Spring boot";
+    }
+}
